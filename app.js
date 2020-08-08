393888da806a875abbea938d04b327066ac52b1f
hisu <hisuwisu@gmail.com>
2020-08-08 07:42:06 +0200

    dhdhf

--- app.js
+++
@@ -1,6 +0,0 @@
-{
-"storage":[
-{"KEY" : "FIRST",   "VALUE" : "26111981", "NAME" : "Tom" },
-{"KEY" : "17122004", "VALUE" : "Album", "NAME" : "17122004" },
-{"KEY" : "LAST",  "VALUE" : "15082020", "NAME" : "Ferdin" }
-]}
